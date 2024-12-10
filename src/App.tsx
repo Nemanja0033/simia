3904b67a1da5d1b717d62bba5c97c92f42e99eb7
nemanja <antonijevicnemanja68@gmail.com>
2024-12-10 16:02:02 +0100

    Auth config

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Admin from "./pages/Admin"
 import ProtectedRoute from "./utils/ProtectedRoute"
+import Groops from "./pages/Groups"
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/admin" element={<ProtectedRoute><Admin /></ProtectedRoute>} />
+        <Route path="/groups" element={<Groops />} />
       </Routes>
     </BrowserRouter>
   )
