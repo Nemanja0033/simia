1102c5927ebdc633ecae5281477af08952a0daed
nemanja <antonijevicnemanja68@gmail.com>
2024-12-18 23:27:32 +0100

    FEAT: Displaying groups cards in groups section

---
+++ src/componnets/user/GruopCard.tsx
@@ -0,0 +1,22 @@
+import { DoorOpenIcon, Users } from "lucide-react"
+
+const GruopCard = ({name, description, members}: {name: string, description: string, members: []}) => {
+  return (
+    <div className="lg:w-64 w-full h-auto shadow-lg flex-row mt-12">
+        <div className="flex justify-center mb-2">
+            <i className="font-bold text-xl text-primary">{name}</i>
+        </div>
+        <div className="flex justify-center items-center">
+          <Users size={16} className="text-primary" />{members.length}
+        </div>
+        <div className="flex justify-center overflow-auto mb-2 h-52 items-center">
+            <p className="text-md text-center text-gray-500">{description}</p>
+        </div>
+        <div className="flex justify-center mb-3">
+            <button className="btn-sm btn btn-neutral text-black bg-primary hover:text-primary">Join Group <DoorOpenIcon /></button>
+        </div>
+    </div>
+  )
+}
+
+export default GruopCard
--- src/pages/Groups.tsx
+++ src/pages/Groups.tsx
@@ -1,7 +1,23 @@
+import { collection, getDocs } from "firebase/firestore"
 import { PlusIcon } from "lucide-react"
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import { db } from "../../config/firebase"
+import GruopCard from "../componnets/user/GruopCard"
 
 const Groups = () => {
+  const [groups, setGroups] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchGroups = async () => {
+      const groupsCollectionRef = collection(db, "groups");
+      const data = await getDocs(groupsCollectionRef);
+      setGroups(data.docs.map((doc) => ({ ...doc.data(), id: doc.id})));
+    }
+    
+    fetchGroups();
+  }, [groups]);
+
   return (
     <div className="w-full h-screen flex justify-center">
       <div className="flex-row mt-6">
@@ -9,6 +25,12 @@ const Groups = () => {
           <Link to={'/creategroup'}>
             <span className="flex gap-2 text-primary hover:text-green-500 cursor-pointer">Create New Group<PlusIcon /></span>
           </Link>
+        </div>
+
+        <div className={`grid gap-8 justify-evenly ${groups.length <= 4 ? 'grid-cols-4' : 'md:grid-cols-3 grid-cols-1'}`}>
+          {groups.map((g) => (
+            <GruopCard name={g.name} description={g.description} members={g.members} />
+          ))}
         </div>
       </div>
     </div>
