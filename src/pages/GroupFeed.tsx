df23730babee530c4e0310647cb03a46d4712786
nemanja <antonijevicnemanja68@gmail.com>
2024-12-20 15:45:07 +0100

    FEAT: group feed UI

--- src/pages/GroupFeed.tsx
+++ src/pages/GroupFeed.tsx
@@ -2,6 +2,7 @@ import { collection, getDocs, query, where } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { db } from "../../config/firebase";
+import { Users } from "lucide-react";
 
 const GroupFeed = () => {
 
@@ -24,8 +25,12 @@ const GroupFeed = () => {
         {feed.map((f) => (
             <div className="flex w-full justify-center">
                 <nav className="md:w-1/2 w-full flex justify-around h-[50px] rounded-md shadow-md gap-4 items-center">
-                <b className="text-primary text-xl">{f.name}</b> 
-                <button className="btn btn-neutral btn-sm bg-primary text-white hover:text-primary border-none">+ NEW BLOG POST</button>               
+                <b className="text-primary text-xl flex gap-2 items-center">{f.name} <span className="flex items-center text-sm"><Users size={16} />({f.members.length})</span></b> 
+                {f.members.length < 2 ? 
+                <button disabled className="btn btn-neutral btn-xs bg-primary text-white hover:text-primary border-none">+ NEW BLOG POST</button> 
+                :
+                <button className="btn btn-neutral btn-xs bg-primary text-white hover:text-primary border-none">+ NEW BLOG POST</button> 
+                }              
                 </nav>
             </div>
         ))}
