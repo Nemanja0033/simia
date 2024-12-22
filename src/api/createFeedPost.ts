1ff09a582fd6447824ad39f49557edf85c2b665a
nemanja <antonijevicnemanja68@gmail.com>
2024-12-22 16:52:41 +0100

    FEAT: feed post logic

---
+++ src/api/createFeedPost.ts
@@ -0,0 +1,25 @@
+import { addDoc, collection } from "firebase/firestore"
+import { auth, db } from "../../config/firebase"
+
+export const createFeedPost = async (heading: string, content: string) => {
+    try{
+        const postCollectionRef = collection(db, 'groupPosts');
+        await addDoc(postCollectionRef, {
+        heading: heading,
+        content: content,
+        author: auth.currentUser?.displayName,
+        createdAt: new Date().toLocaleString('en-US', {
+            weekday: 'long',
+            hour: '2-digit',
+            minute: '2-digit',
+          }),
+        likes: {
+            count: 0,
+            users: [],
+        }
+    })
+    }
+    catch(err){
+        console.log('Error while creating post', err);
+    }
+}
--- src/pages/GroupFeed.tsx
+++ src/pages/GroupFeed.tsx
@@ -6,12 +6,15 @@ import { Check, Info, ShieldCheck, Users } from "lucide-react";
 import Loader from "../ui/Loader";
 import { useMember } from "../context/memberContext";
 import { acceptMember } from "../api/acceptMember";
+import { createFeedPost } from "../api/createFeedPost";
 
 const GroupFeed = () => {
     const { groupID } = useParams<{ groupID: string }>();
     const [loading, setLoading] = useState<boolean>(true);
     const [feed, setFeed] = useState<any[]>([]);
     const [memberRequest, setMemeberRequest] = useState<any[]>([]);
+    const [heading, setHeading] = useState<string>("");
+    const [content, setContent] = useState<string>("");
     const { isMember } = useMember();
 
     const IS_MEMBER = (name: string) => isMember === name;
@@ -36,8 +39,8 @@ const GroupFeed = () => {
     }, [groupID]);
 
 
-    const openModal = () => {
-        const modal = document.getElementById('my_modal_2') as HTMLDialogElement | null;
+    const openModal = (id: string) => {
+        const modal = document.getElementById(id) as HTMLDialogElement | null;
         if (modal) {
           modal.showModal();
         } else {
@@ -64,14 +67,31 @@ const GroupFeed = () => {
                         :
                         <button className="btn btn-neutral btn-xs bg-primary text-white hover:text-primary border-none">+ NEW BLOG POST</button> 
                         }
-                        <button className="btn btn-neutral btn-xs bg-primary text-white hover:text-primary border-none">Post to feed</button>               
+                        <button className="btn btn-neutral btn-xs bg-primary text-white hover:text-primary border-none" onClick={() => openModal('my_modal_1')}>Post to feed</button>               
+                        <dialog id="my_modal_1" className="modal">
+                          <div className="modal-box">
+                          <h1 className="font-bold text-primary text-xl text-center mb-3">Share on Feed</h1>
+                            <div className="flex justify-center items-center">
+                              <input className="focus:ring-2 focus:ring-primary w-full focus:outline-none rounded-md border border-primary" type="text" placeholder="Post Heading. . ." value={heading} onChange={(e) => {setHeading(e.target.value)}} />
+                            </div>
+                            <div className="flex justify-center mt-3">
+                              <textarea className="focus:ring-2 focus:ring-primary w-full min-h-32 focus:outline-none rounded-md border border-primary" placeholder="Your content. . ." value={content} onChange={(e) => {setContent(e.target.value)}} />
+                            </div>
+                            <div className="flex justify-center mt-3">
+                              <button onClick={() => createFeedPost(heading, content)} className="btn-neutral btn bg-primary text-white w-full hover:text-primary border-none">Post</button>
+                            </div>
+                          </div>
+                          <form method="dialog" className="modal-backdrop">
+                            <button>Close</button>
+                          </form>
+                        </dialog>
                         {IS_MODERATOR(f.groupID) ? // check is current user a moderator of group
                         <div className="flex items-center">
-                        <button className="btn btn-xs btn-neutral border-none bg-primary text-white hover:text:primary" onClick={() => fetchMemberRequests(f.name)} onDoubleClick={openModal}><ShieldCheck /></button>
+                        <button className="btn btn-xs btn-neutral border-none bg-primary text-white hover:text:primary" onClick={() => fetchMemberRequests(f.name)} onDoubleClick={() => openModal('my_modal_2')}><ShieldCheck /></button>
                         <dialog id="my_modal_2" className="modal">
                           <div className="modal-box">
                             <div className="ml-12">
-                            <p className="font-bold text-primary text-xl text-center">Member Requests</p>
+                            <h1 className="font-bold text-primary text-xl text-center">Member Requests</h1>
                             {memberRequest.map((m) => (
                               <div className="flex justify-center gap-3 items-center text-md mt-3">
                                 <span className="font-bold">{m.username}</span>
