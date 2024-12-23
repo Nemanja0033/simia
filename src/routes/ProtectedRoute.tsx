92a5c163978903abd5fcefaad37fb41a338be496
nemanja <antonijevicnemanja68@gmail.com>
2024-12-23 12:59:23 +0100

    FIX: route protection

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,12 +1,16 @@
 
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/authContext";
+import { auth } from "../../config/firebase";
 
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
     const { isActive } = useAuth();
 
-    if (!isActive) {
+    if (!auth) {
         return <Navigate to="/login" />;
+    }
+    if(!isActive){
+        return <div className="w-full h-screen flex justify-center items-center"><h1 className="text-xl text-center">Your account is currently pending review by an administrator for verification. We appreciate your patience during this process <a className="underline text-primary" href="/">Back Home</a></h1></div>
     }
 
     return children;
