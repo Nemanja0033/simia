96fea62a103f04bd6e98f002ce26f280294a1bcb
nemanja <antonijevicnemanja68@gmail.com>
2024-12-29 02:13:10 +0100

    BUILD

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,20 +3,27 @@ import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../../config/firebase';
 import BlogPostCard from '../componnets/user/BlogPostCard';
 import { ArrowDown } from 'lucide-react';
+import Loader from '../ui/Loader';
 
 const Home = () => {
   
   const [blog, setBlog] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchBlogs = async () => {
       const blogCollectionRef = collection(db, 'blogPosts');
       const data = await getDocs(blogCollectionRef);
       setBlog(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setLoading(false);
     }
 
     fetchBlogs();
   }, []);
+
+  if(loading){
+    return <Loader />
+  }
 
   return (
     <div className='md:mt-20 mt-32 w-full flex flex-col items-center'>
