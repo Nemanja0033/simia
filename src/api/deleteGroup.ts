a0b3bb77c6b19d1c42379bdb95190d0db91ff6f5
nemanja <antonijevicnemanja68@gmail.com>
2025-01-04 15:51:51 +0100

    FEAT: Deleting groups and clear members

---
+++ src/api/deleteGroup.ts
@@ -0,0 +1,51 @@
+import { collection, deleteDoc, getDocs, query, updateDoc, where } from "firebase/firestore";
+import { db } from "../../config/firebase";
+
+export const deleteGroup = async (groupID: string, groupName: string) => {
+    const confirmMsg = confirm("Are you sure you want to delete this group including all members and data?");
+    if (!confirmMsg) return;
+
+    try {
+        // Brisanje grupe
+        const groupCollectionRef = collection(db, "groups");
+        const groupQuery = query(groupCollectionRef, where("groupID", "==", groupID));
+        const groupSnapshot = await getDocs(groupQuery);
+
+        if (groupSnapshot.empty) {
+            console.error(`No group found with ID: ${groupID}`);
+            alert("Group not found. Please check the ID.");
+            return;
+        }
+
+        const deleteGroupPromises = groupSnapshot.docs.map((doc) =>
+            deleteDoc(doc.ref).catch((err) => {
+                console.error(`Failed to delete document with ID ${doc.id}:`, err);
+            })
+        );
+        await Promise.all(deleteGroupPromises);
+
+        console.log("Group documents successfully deleted.");
+
+        // Ažuriranje korisnika
+        const userCollectionRef = collection(db, "users");
+        const userQuery = query(userCollectionRef, where("group", "==", groupName));
+        const userSnapshot = await getDocs(userQuery);
+
+        if (!userSnapshot.empty) {
+            const updateUserPromises = userSnapshot.docs.map((userDoc) =>
+                updateDoc(userDoc.ref, {
+                    group: "",
+                    moderator: "",
+                }).catch((err) => {
+                    console.error(`Failed to update user document with ID ${userDoc.id}:`, err);
+                })
+            );
+            await Promise.all(updateUserPromises);
+            console.log("User documents successfully updated.");
+        } else {
+            console.log("No users found to update.");
+        }
+    } catch (err) {
+        console.error("Error while deleting group:", err);
+    }
+};
--- src/componnets/admin/Dashboard.tsx
+++ src/componnets/admin/Dashboard.tsx
@@ -1,12 +1,21 @@
 import { collection, getDocs, query, where } from "firebase/firestore";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { db } from "../../../config/firebase";
 import Loader from "../../ui/Loader";
-import { AlarmClockCheck, Check, Group, History, Trash, Users, X } from "lucide-react";
+import {
+  AlarmClockCheck,
+  Check,
+  Group,
+  History,
+  Trash,
+  Users,
+  X,
+} from "lucide-react";
 import { activateUser } from "../../api/activateUser";
 import { deactivateUser } from "../../api/deactivateUser";
 import { deleteRequest } from "../../api/deleteRequest";
 import { clearUserHistory } from "../../api/clearUserHistory";
+import { deleteGroup } from "../../api/deleteGroup";
 
 const Dashboard = () => {
   const [activeUsers, setActiveUsers] = useState<any[]>([]);
@@ -18,190 +27,207 @@ const Dashboard = () => {
   const [groupSearch, setGroupSearch] = useState<string>("");
   const [userSearch, setUserSearch] = useState<string>("");
 
-  const fetchUsers = async () => {
+  const fetchData = async () => {
     try {
-      const usersCollectionRef = collection(db, "users");
-      const q = query(usersCollectionRef, where("status", "==", "active"));
-      const data = await getDocs(q);
-      setActiveUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    } catch (err) {
-      console.error("Error while fetching users data", err);
-    } finally {
-      setLoading(false);
-    }
-  };
+      setLoading(true);
+      const [users, requests, histories, groupsData] = await Promise.all([
+        getDocs(query(collection(db, "users"), where("status", "==", "active"))),
+        getDocs(
+          query(collection(db, "users"), where("status", "in", ["pending", "inactive"]))
+        ),
+        getDocs(collection(db, "history")),
+        getDocs(collection(db, "groups")),
+      ]);
 
-  const fetchUserRequests = async () => {
-    try {
-      const requestsCollectionRef = collection(db, "users");
-      const q = query(
-        requestsCollectionRef,
-        where("status", "in", ["pending", "inactive"])
-      );
-      const data = await getDocs(q);
-      setPendingUsers(
-        data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
-      );
-    } catch (err) {
-      console.error("Error while fetching user requests", err);
-    }
-  };
-
-  const fetchHistory = async () => {
-    try {
-      const historyCollectionRef = collection(db, "history");
-      const data = await getDocs(historyCollectionRef);
-      setHistory(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    } catch (err) {
-      console.error("Error while fetching users data", err);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const fetchGroups = async () => {
-    try {
-      const groupsCollectionRef = collection(db, "groups");
-      const data = await getDocs(groupsCollectionRef);
-      setGroups(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    } catch (err) {
-      console.log("Error while fetching groups", err);
+      setActiveUsers(users.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setPendingUsers(requests.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setHistory(histories.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setGroups(groupsData.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    } catch (error) {
+      console.error("Error fetching data:", error);
     } finally {
       setLoading(false);
     }
   };
 
   useEffect(() => {
-      const callFunctions = async () => {
-        try {
-          await Promise.all([
-            fetchHistory(),
-            fetchUsers(),
-            fetchUserRequests(),
-            fetchGroups()
-          ]);
-        } catch (error) {
-          console.error("Error fetching data:", error);
-        }
-      };
+    fetchData();
+  }, []);
 
-      callFunctions();
-  }, []); //empty array to prevent endless loop of http requests 
-
-  if(loading){
-    return <Loader />
+  if (loading) {
+    return <Loader />;
   }
 
   return (
     <div className="w-full h-screen md:flex flex-row justify-around items-center md:mt-10 mt-32">
-        <div className="md:w-1/3 w-full h-96 m-3 rounded-md shadow-md">
-             <h1 className="text-center font-bold flex justify-center gap-2 items-center text-primary"><Users />Users ({activeUsers.length})</h1>
-             <div className="flex justify-center mt-3 items-center gap-2">
-                <input onChange={(e) => {setUserSearch(e.target.value)}} className="focus:ring-2 focus:ring-primary focus:outline-none rounded-md border border-primary" type="text" placeholder="Search user by username" />
-             </div>
-             <div className="flex-row overflow-auto h-72 justify-self-center mt-6">
-              {activeUsers.filter((item) =>
-                  userSearch.toLowerCase() === ""
-                  ? true
-                  : item.username.toLowerCase().includes(userSearch.toLowerCase())
-                ).map((u) => (
-                <ul key={u.userID} className="mb-6 text-center">
-                  <li><b>Username</b>: <i>{u.username}</i></li>
-                  <li><b>User Email</b>: <i>{u.email}</i></li>
-                  <li><b>User ID</b>: <i>{u.userID}</i></li>
-                  <li><b>User Status</b>: <i>{u.status}</i></li>
-                  <div className="flex justify-center gap-2">
-                  <button onClick={() => deactivateUser(u.userID)} className="flex gap-2 text-red-400 hover:text-red-600"><X />Disable User</button>
-                  </div>
-                </ul>
-              ))}
-             </div>
+      {/* Active Users Section */}
+      <div className="md:w-1/3 w-full h-96 m-3 rounded-md shadow-md">
+        <h1 className="text-center font-bold flex justify-center gap-2 items-center text-primary">
+          <Users /> Users ({activeUsers.length})
+        </h1>
+        <div className="flex justify-center mt-3 items-center gap-2">
+          <input
+            onChange={(e) => setUserSearch(e.target.value)}
+            className="focus:ring-2 focus:ring-primary focus:outline-none rounded-md border border-primary"
+            type="text"
+            placeholder="Search user by username"
+          />
         </div>
-  
-        <div className="md:w-1/3 flex-row w-full h-96 m-3 rounded-md shadow-md">
-            <h1 className="text-center font-bold flex justify-center gap-2 items-center text-primary"><AlarmClockCheck />Pending Users ({pendingUsers.length})</h1> 
-            <div className="flex-row overflow-auto h-80 justify-self-center mt-6">
-            {pendingUsers.length === 0 ? (
-              <h1 className="text-center font-bold">There is no pending users</h1>
-            ) : (
-              pendingUsers.map((p) => (
-                <ul key={p.email} className="mb-6 text-center">
-                  <li><b>Username</b>: <i>{p.username}</i></li>
-                  <li><b>Email</b>: <i>{p.email}</i></li>
-                  <li><b>User Status:</b> <i>{p.status}</i></li>
-                  <div className="flex justify-center gap-2">
-                    <button 
-                      onClick={() => activateUser(p.userID)} 
-                      className="flex gap-2 text-primary hover:text-green-700"
-                    >
-                      <Check /> Approve
-                    </button>
-                    <button 
-                      onClick={() => deleteRequest(p.userID)} 
-                      className="flex gap-2 text-red-400 hover:text-red-600"
-                    >
-                      <X /> Decline
-                    </button>
-                  </div>
-                </ul>
-              ))
-            )}
-            </div>
-        </div>
-
-        <div className="md:w-1/3 w-full h-96 m-3 rounded-md shadow-md">
-             <h1 className="text-center font-bold flex justify-center gap-2 items-center text-primary"><Group />Groups ({groups.length})</h1>
-             <div className="flex justify-center mt-3 items-center gap-2">
-                <input onChange={(e) => {setGroupSearch(e.target.value)}} className="focus:ring-2 focus:ring-primary focus:outline-none rounded-md border border-primary" type="text" placeholder="    Search group by name" />
-             </div>
-             <div className="flex-row w-full overflow-auto h-72 justify-self-center mt-6">
-              {groups.filter((item) => groupSearch.toLowerCase() === ''
-                ? item
-                : item.name.toLowerCase().includes(groupSearch.toLowerCase())
-              ).map((g) => (
-                <div className="flex-row w-full h-auto mb-3">
-                  <div className="flex justify-center">
-                    <span className="font-bold text-primary">{g.name}</span>
-                  </div>
-                  <h1 className="text-center font-semibold">Members ({g.members.length})</h1>
-                  <div className="h-32 justify-center">
-                    {g.members.map((member: string, index: number) => (
-                      <ul key={index} className="flex justify-center">
-                        <li>{member}</li>
-                      </ul>
-                    ))}
-                  </div>
+        <div className="flex-row overflow-auto h-72 justify-self-center mt-6">
+          {activeUsers
+            .filter((item) =>
+              userSearch.toLowerCase() === ""
+                ? true
+                : item.username.toLowerCase().includes(userSearch.toLowerCase())
+            )
+            .map((u) => (
+              <ul key={u.userID} className="mb-6 text-center">
+                <li>
+                  <b>Username</b>: <i>{u.username}</i>
+                </li>
+                <li>
+                  <b>User Email</b>: <i>{u.email}</i>
+                </li>
+                <li>
+                  <b>User ID</b>: <i>{u.userID}</i>
+                </li>
+                <li>
+                  <b>User Status</b>: <i>{u.status}</i>
+                </li>
+                <div className="flex justify-center gap-2">
+                  <button
+                    onClick={() => deactivateUser(u.userID)}
+                    className="flex gap-2 text-red-400 hover:text-red-600"
+                  >
+                    <X /> Disable User
+                  </button>
                 </div>
-              ))}
-             </div>
+              </ul>
+            ))}
         </div>
+      </div>
 
-        <div className="md:w-1/3 flex-row w-full h-96 m-3 rounded-md shadow-md">
-        <h1 className="text-center font-bold flex justify-center gap-2 items-center text-primary"><History />Login History ({history.length})</h1>
-            <div className="flex-row overflow-auto h-72 justify-self-center mt-4">
-               {history.length === 0 ? (
-                <h1 className="text-center font-bold">Login history is clear</h1>
-               )
-              :
-              (
-                history.map((h) => (
-                  <ul key={h.email} className="mb-6 text-center">
-                    <li><b>Email</b>: <i>{h.email}</i></li>
-                    <li><b>At</b>: <i>{h.at}</i></li>
-                  </ul>
-                ))
-              )}
-            </div>
-          {history.length === 0 ? '' :
-          (
-            <div className="flex justify-center mt-2">
-            <button onClick={clearUserHistory} className="text-red-400 hover:text-red-600 flex gap-2 items-center"><Trash size={20} />Clear History</button>
-          </div>
+      {/* Pending Users Section */}
+      <div className="md:w-1/3 flex-row w-full h-96 m-3 rounded-md shadow-md">
+        <h1 className="text-center font-bold flex justify-center gap-2 items-center text-primary">
+          <AlarmClockCheck /> Pending Users ({pendingUsers.length})
+        </h1>
+        <div className="flex-row overflow-auto h-80 justify-self-center mt-6">
+          {pendingUsers.length === 0 ? (
+            <h1 className="text-center font-bold">There is no pending users</h1>
+          ) : (
+            pendingUsers.map((p) => (
+              <ul key={p.email} className="mb-6 text-center">
+                <li>
+                  <b>Username</b>: <i>{p.username}</i>
+                </li>
+                <li>
+                  <b>Email</b>: <i>{p.email}</i>
+                </li>
+                <li>
+                  <b>User Status:</b> <i>{p.status}</i>
+                </li>
+                <div className="flex justify-center gap-2">
+                  <button
+                    onClick={() => activateUser(p.userID)}
+                    className="flex gap-2 text-primary hover:text-green-700"
+                  >
+                    <Check /> Approve
+                  </button>
+                  <button
+                    onClick={() => deleteRequest(p.userID)}
+                    className="flex gap-2 text-red-400 hover:text-red-600"
+                  >
+                    <X /> Decline
+                  </button>
+                </div>
+              </ul>
+            ))
           )}
         </div>
-    </div>
-    
-  )
-}
+      </div>
 
-export default Dashboard
+      {/* Groups Section */}
+      <div className="md:w-1/3 w-full h-96 m-3 rounded-md shadow-md">
+        <h1 className="text-center font-bold flex justify-center gap-2 items-center text-primary">
+          <Group /> Groups ({groups.length})
+        </h1>
+        <div className="flex justify-center mt-3 items-center gap-2">
+          <input
+            onChange={(e) => setGroupSearch(e.target.value)}
+            className="focus:ring-2 focus:ring-primary focus:outline-none rounded-md border border-primary"
+            type="text"
+            placeholder="Search group by name"
+          />
+        </div>
+        <div className="flex-row w-full overflow-auto h-72 justify-self-center mt-6">
+          {groups
+            .filter((g) => g.groupID && g.name) // Filtriraj grupe koje imaju groupID i groupName
+            .filter((g) =>
+              groupSearch.toLowerCase() === ""
+                ? true
+                : g.groupName.toLowerCase().includes(groupSearch.toLowerCase())
+            )
+            .map((g) => (
+              <div key={g.id} className="flex-row w-full h-auto mb-3">
+                <div className="flex gap-2 justify-center">
+                  <span className="font-bold text-primary">{g.name}</span>
+                  <button
+                    className="text-red-400 hover:text-red-600"
+                    onClick={() => deleteGroup(g.groupID, g.name)}
+                  >
+                    <Trash />
+                  </button>
+                </div>
+                <h1 className="text-center font-semibold">
+                  Members ({g.members?.length || 0})
+                </h1>
+                <div className="h-32 justify-center">
+                  {g.members?.map((member: string, index: number) => (
+                    <ul key={index} className="flex justify-center">
+                      <li>{member}</li>
+                    </ul>
+                  ))}
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+
+      {/* Login History Section */}
+      <div className="md:w-1/3 flex-row w-full h-96 m-3 rounded-md shadow-md">
+        <h1 className="text-center font-bold flex justify-center gap-2 items-center text-primary">
+          <History /> Login History ({history.length})
+        </h1>
+        <div className="flex-row overflow-auto h-72 justify-self-center mt-4">
+          {history.length === 0 ? (
+            <h1 className="text-center font-bold">Login history is clear</h1>
+          ) : (
+            history.map((h) => (
+              <ul key={h.email} className="mb-6 text-center">
+                <li>
+                  <b>Email</b>: <i>{h.email}</i>
+                </li>
+                <li>
+                  <b>At</b>: <i>{h.at}</i>
+                </li>
+              </ul>
+            ))
+          )}
+        </div>
+        {history.length > 0 && (
+          <div className="flex justify-center mt-2">
+            <button
+              onClick={clearUserHistory}
+              className="text-red-400 hover:text-red-600 flex gap-2 items-center"
+            >
+              <Trash size={20} /> Clear History
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
